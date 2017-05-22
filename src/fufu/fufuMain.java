6f7e1e4c3ffa543434f0a186dd191e99759d3e6b
ppark0723 <ppark0723@naver.com>
2017-05-22 17:25:27 +0900

    plzzzzzzz123
    plzza123

--- src/fufu/fufuMain.java
+++ src/fufu/fufuMain.java
@@ -5,5 +5,18 @@ public class fufuMain {
 		System.out.println("fufu main!!");
 		System.out.println("ppark ");
 		System.out.println("plzzzzzzz");
+		System.out.println("fufu main!!");
+		System.out.println("ppark ");
+		System.out.println("plzzzzzzz");
+		System.out.println("fufu main!!");
+		System.out.println("ppark ");
+		System.out.println("plzzzzzzz");
+		System.out.println("fufu main!!");
+		System.out.println("ppark ");
+		System.out.println("plzzzzzzz");
+		
+		System.out.println("fufu main!!");
+		System.out.println("ppark ");
+		System.out.println("plzzzzzzz");
 	}
 }
